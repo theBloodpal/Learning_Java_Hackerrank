669cfabcc0287a2be12f058d1c02392f6436dcd3
Palash Indurkar <67363517+theBloodpal@users.noreply.github.com>
2023-07-02 00:48:45 +0530

    Create if_else.java

---
+++ if_else.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        if( n % 2 == 0){
+            if(n >=2 && n <=5 ){
+                System.out.println("Not Weird");
+            }
+            else if(n >= 6 && n <= 20){
+                System.out.println("Weird");
+            }
+            else if(n > 20){
+                System.out.println("Not Weird");
+            }
+        }
+        else{
+            System.out.println("Weird");
+        }
+
+
+        bufferedReader.close();
+    }
+}
